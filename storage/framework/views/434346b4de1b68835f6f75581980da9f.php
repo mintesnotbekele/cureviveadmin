47340081939ada22c2e9127b687dc3ed732245ca
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2023-08-21 11:10:06 +0300

    added csrf token

---
+++ storage/framework/views/434346b4de1b68835f6f75581980da9f.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Page Expired')); ?>
+<?php $__env->startSection('code', '419'); ?>
+<?php $__env->startSection('message', __('Page Expired')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /media/mente/F4A81F8DA81F4D8C/projects/prophecius/curevive-admin-panel/resources/views/errors/419.blade.php ENDPATH**/ ?>
